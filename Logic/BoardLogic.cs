b89205b903f2aaf8b3c02af27b5c563d7b68ddac
Kopec321 <164060518+Kopec321@users.noreply.github.com>
2024-04-19 21:14:44 +0200

    Added BoardLogic and BoardLogicAPI and functions CreateBall and RemoveBall

---
+++ Logic/BoardLogic.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+
+namespace Logic
+{
+    internal class BoardLogic : BoardLogicAPI
+    {
+        private Vector2 dimensions;
+        private readonly List<BallAPI> balls = [];
+        public BoardLogic(float x, float y)
+        {
+            dimensions = new Vector2(x, y);
+        }
+
+        public override void CreateBall(float radius)
+        {
+            Random random = new Random();
+            float x = ((float)random.NextDouble() * (dimensions.X - radius)) + radius;
+            float y = ((float)random.NextDouble() * (dimensions.Y - radius)) + radius;
+
+            float xVelocity = ((float)random.NextDouble() * 5 + 1) * (2 * random.Next(0,1) - 1);
+            float yVelocity = random.Next(-3, 3);
+            balls.Add(BallAPI.GetBall(x, y, xVelocity, yVelocity, radius));
+
+
+        }
+        public override void RemoveBall()
+        {
+            if (balls.Count > 0)
+            {
+                Random random = new();
+
+                balls.RemoveAt(random.Next(balls.Count));
+            }
+        }
+
+    }
+}
