895a65ded42e95aacc30b4874510be24adc6ca74
Kopec321 <164060518+Kopec321@users.noreply.github.com>
2024-04-20 16:55:57 +0200

    removed dimension getter from BoardLogic

--- Logic/BoardLogic.cs
+++ Logic/BoardLogic.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Numerics;
 using Data;
 
 namespace Logic
@@ -23,21 +18,20 @@ namespace Logic
             float x = ((float)random.NextDouble() * (dimensions.X - radius)) + radius;
             float y = ((float)random.NextDouble() * (dimensions.Y - radius)) + radius;
 
-            float xVelocity = ((float)random.NextDouble() * 5 + 1) * (2 * random.Next(0,1) - 1);
-            float yVelocity = random.Next(-3, 3);
+            float xVelocity = ((float)random.NextDouble() * 5 + 1) * (2 * random.Next(0, 1) - 1);
+            float yVelocity = ((float)random.NextDouble() * 5 + 1) * (2 * random.Next(0, 1) - 1);
+
             balls.Add(BallAPI.GetBall(x, y, xVelocity, yVelocity, radius));
-
-
         }
+
         public override void RemoveBall()
         {
-            if (balls.Count > 0)
+            if (balls.Count > 0) 
             {
                 Random random = new();
 
                 balls.RemoveAt(random.Next(balls.Count));
             }
         }
-
     }
 }
--- Logic/BoardLogicAPI.cs
+++ Logic/BoardLogicAPI.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Logic
+﻿namespace Logic
 {
-    internal class BoardLogicAPI
+    public abstract class BoardLogicAPI
     {
+        public static BoardLogicAPI GetBoardLogic(float x, float y)
+        {
+            return new BoardLogic(x, y);
+        }
+        public abstract void CreateBall(float radius);
+
+        public abstract void RemoveBall();
     }
 }
