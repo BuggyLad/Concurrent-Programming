8092f05f0ec48afce7345bb0b32919d322391461
Kopec321 <164060518+Kopec321@users.noreply.github.com>
2024-04-23 19:20:42 +0200

    Changes in ModelAbstractAPI

--- Model/ModelAbstractAPI.cs
+++ Model/ModelAbstractAPI.cs
@@ -6,9 +6,8 @@ namespace Presentation.Model
     public abstract class ModelAbstractAPI
     {
         public abstract void CreateBall();
+        public abstract void RemoveBall();
         public abstract ObservableCollection<BallLogicAbstractAPI> GetBalls();
-        public abstract float Radius { get;}
-        public abstract float Diameter { get; }
         public static ModelAbstractAPI CreateAPI()
         {
             return new ModelAPI();
